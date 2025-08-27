f665d6e48a01fddfbf26c34e2917f93219f0e05b
Xebec19 <rohandeveloper106@gmail.com>
2025-08-27 22:33:49 +0530

    feat(web): upload post

---
+++ web/app/post/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { CreatePostPage } from "@/components/create-post-page";
+import React from "react";
+
+function page() {
+  return (
+    <div className="dark">
+      <CreatePostPage />
+    </div>
+  );
+}
+
+export default page;
