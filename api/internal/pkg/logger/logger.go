fde7b8cfbd99c26dc1f49ce317e8c43d5d31457d
Xebec19 <rohandeveloper106@gmail.com>
2025-09-01 21:57:01 +0530

    feat: logger

---
+++ api/internal/pkg/logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+var log *slog.Logger
+
+// Init sets up the default logger for the app.
+func Init(level slog.Level, format string) {
+	opts := &slog.HandlerOptions{Level: level}
+
+	var h slog.Handler
+	if format == "text" {
+		h = slog.NewTextHandler(os.Stdout, opts)
+	} else {
+		h = slog.NewJSONHandler(os.Stdout, opts)
+	}
+
+	log = slog.New(h)
+	slog.SetDefault(log) // optional: replace global slog
+}
+
+// Exposed helpers
+func Info(msg string, args ...any)  { log.Info(msg, args...) }
+func Warn(msg string, args ...any)  { log.Warn(msg, args...) }
+func Error(msg string, args ...any) { log.Error(msg, args...) }
+func Debug(msg string, args ...any) { log.Debug(msg, args...) }
